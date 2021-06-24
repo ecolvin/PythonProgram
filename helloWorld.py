166a119e00413e1ecff511d04845dd921efc0b7e
ecolvin <ecolvin@mail.csuchico.edu>
2021-06-24 01:57:48 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+data = [
+  {
+    'id': 0,
+    'First Name': 'Eric',
+    'Last Name': 'Colvin'
+  },
+  {
+    'id':1,
+    'First Name': 'John',
+    'Last Name': 'Doe'
+  },
+]
+
+@app.route('/', methods=['GET'])
+def home():
+  return jsonify(data)
+
+app.run()
